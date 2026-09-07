5d29e60714bda4e98d1328664d2b3a2ad63af7d0
dmonyo <devel.enterprise@gmail.com>
2026-09-06 21:43:57 -0400

    comment fucntion for documentation

--- workers/match.worker.ts
+++ workers/match.worker.ts
@@ -11,7 +11,7 @@ export type MatchWorkerRequest = {
   maxResultRows?: number
 }
 
-
+/** Response from the match worker. */
 export type MatchWorkerResponse =
   | { type: "progress"; p: number }
   | { type: "done"; result: MatchResult }
