e10a9fc3eceeea7d8e7edbf9476e160f380109c4
dmonyo <devel.enterprise@gmail.com>
2026-09-06 21:44:44 -0400

    remover comment

--- workers/match.worker.ts
+++ workers/match.worker.ts
@@ -11,7 +11,6 @@ export type MatchWorkerRequest = {
   maxResultRows?: number
 }
 
-/** Response from the match worker. */
 export type MatchWorkerResponse =
   | { type: "progress"; p: number }
   | { type: "done"; result: MatchResult }
